45dc97e1ed1e755c8d35a13c7f0588151b4af534
_ <93497294+shelbernstein@users.noreply.github.com>
2023-02-04 16:56:16 -0600

    hello world

--- demos/postProcessing.js
+++ demos/postProcessing.js
@@ -110,74 +110,293 @@ fn frag_main(@location(0) fragUV : vec2<f32>) -> @location(0) vec4<f32> {
 }
 `
 async function postProcessing() {
-  let webgpu = await webgpuInit()
-  let device = webgpu.device;
-
-  let img = new Image();
-  img.src = '../data/webgpu.png'
-  let cubeTexture = await webgpu.texture(img)
-  console.log(cubeTexture.texture)
-  const [srcWidth, srcHeight] = [cubeTexture.width, cubeTexture.height];
-  const textures = [
-    (await webgpu.texture([srcWidth, srcHeight])).texture,
-    (await webgpu.texture([srcWidth, srcHeight])).texture,
-  ]
-
-  const draw = await webgpu.initDrawCall({
-    label: 'postprocess-draw',
-    shader: { code: fullscreenTexturedQuadWGSL,
-              fragEntryPoint: "frag_main",
-              vertEntryPoint: "vert_main"
-    },
-    bindGroup: ({pipeline}) => { 
-    return [pipeline.getBindGroupLayout(0), [cubeTexture.sampler, cubeTexture.texture.createView()]]
-    }
-  })
-
-  const blurParamsBuffer = utils.paramsBuffer(device)
-  const compute = webgpu.initComputeCall({
-    code: blurWGSL,
-    uniforms: {
-      filterSize: 15
-    },
-    bindGroups: function (state, computePipeline) {
-      const device = state.device;
-      const blurPipeline = computePipeline
-
-      const buffer0 = utils.createBuffer(device, 0)
-      const buffer1 = utils.createBuffer(device, 1)
-      const computeConstants = utils.makeBindGroup(device, blurPipeline.getBindGroupLayout(0), [cubeTexture.sampler, blurParamsBuffer])
-      const computeBindGroup0 = utils.makeBindGroup(device, blurPipeline.getBindGroupLayout(1), [cubeTexture.texture.createView(), textures[0].createView(), buffer0], 1)
-      const computeBindGroup1 = utils.makeBindGroup(device, blurPipeline.getBindGroupLayout(1), [textures[0].createView(),  textures[1].createView(), buffer1,], 1)
-      const computeBindGroup2 = utils.makeBindGroup(device, blurPipeline.getBindGroupLayout(1), [textures[1].createView(),  textures[0].createView(), buffer0,], 1)
-      return [computeConstants, computeBindGroup0, computeBindGroup1, computeBindGroup2]
-    },
-  })
-
-  const settings = {
-    filterSize: 15,
-    iterations: 10,
-  };
-
-  let blockDim;
-  const updateSettings = () => {
-  blockDim = tileDim - (settings.filterSize - 1);
-    device.queue.writeBuffer(
-      blurParamsBuffer,
-      0,
-      new Uint32Array([settings.filterSize, blockDim])
-    );
-  };
-
-  updateSettings();
-
-  function frame() {
-    //compute()
-    draw()
+  const tileDim = 128;
+  const batch = [4, 4];
   
+    const adapter = await navigator.gpu.requestAdapter();
+    const device = await adapter.requestDevice();
+
+    const canvas = utils.createCanvas()
+    const context = canvas.getContext('webgpu');
+  
+    const devicePixelRatio = window.devicePixelRatio || 1;
+    canvas.width = canvas.clientWidth * devicePixelRatio;
+    canvas.height = canvas.clientHeight * devicePixelRatio;
+    const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+  
+    context.configure({
+      device,
+      format: presentationFormat,
+      alphaMode: 'opaque',
+    });
+  
+    const blurPipeline = device.createComputePipeline({
+      layout: 'auto',
+      compute: {
+        module: device.createShaderModule({
+          code: blurWGSL,
+        }),
+        entryPoint: 'main',
+      },
+    });
+  
+    const fullscreenQuadPipeline = device.createRenderPipeline({
+      layout: 'auto',
+      vertex: {
+        module: device.createShaderModule({
+          code: fullscreenTexturedQuadWGSL,
+        }),
+        entryPoint: 'vert_main',
+      },
+      fragment: {
+        module: device.createShaderModule({
+          code: fullscreenTexturedQuadWGSL,
+        }),
+        entryPoint: 'frag_main',
+        targets: [
+          {
+            format: presentationFormat,
+          },
+        ],
+      },
+      primitive: {
+        topology: 'triangle-list',
+      },
+    });
+  
+    const sampler = device.createSampler({
+      magFilter: 'linear',
+      minFilter: 'linear',
+    });
+  
+    const img = document.createElement('img');
+    img.src = new URL(
+      '../data/webgpu.png',
+      import.meta.url
+    ).toString();
+    await img.decode();
+    const imageBitmap = await createImageBitmap(img);
+  
+    const [srcWidth, srcHeight] = [imageBitmap.width, imageBitmap.height];
+    const cubeTexture = device.createTexture({
+      size: [srcWidth, srcHeight, 1],
+      format: 'rgba8unorm',
+      usage:
+        GPUTextureUsage.TEXTURE_BINDING |
+        GPUTextureUsage.COPY_DST |
+        GPUTextureUsage.RENDER_ATTACHMENT,
+    });
+    device.queue.copyExternalImageToTexture(
+      { source: imageBitmap },
+      { texture: cubeTexture },
+      [imageBitmap.width, imageBitmap.height]
+    );
+  
+    const textures = [0, 1].map(() => {
+      return device.createTexture({
+        size: {
+          width: srcWidth,
+          height: srcHeight,
+        },
+        format: 'rgba8unorm',
+        usage:
+          GPUTextureUsage.COPY_DST |
+          GPUTextureUsage.STORAGE_BINDING |
+          GPUTextureUsage.TEXTURE_BINDING,
+      });
+    });
+  
+    const buffer0 = (() => {
+      const buffer = device.createBuffer({
+        size: 4,
+        mappedAtCreation: true,
+        usage: GPUBufferUsage.UNIFORM,
+      });
+      new Uint32Array(buffer.getMappedRange())[0] = 0;
+      buffer.unmap();
+      return buffer;
+    })();
+  
+    const buffer1 = (() => {
+      const buffer = device.createBuffer({
+        size: 4,
+        mappedAtCreation: true,
+        usage: GPUBufferUsage.UNIFORM,
+      });
+      new Uint32Array(buffer.getMappedRange())[0] = 1;
+      buffer.unmap();
+      return buffer;
+    })();
+  
+    const blurParamsBuffer = device.createBuffer({
+      size: 8,
+      usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.UNIFORM,
+    });
+  
+    const computeConstants = device.createBindGroup({
+      layout: blurPipeline.getBindGroupLayout(0),
+      entries: [
+        {
+          binding: 0,
+          resource: sampler,
+        },
+        {
+          binding: 1,
+          resource: {
+            buffer: blurParamsBuffer,
+          },
+        },
+      ],
+    });
+  
+    const computeBindGroup0 = device.createBindGroup({
+      layout: blurPipeline.getBindGroupLayout(1),
+      entries: [
+        {
+          binding: 1,
+          resource: cubeTexture.createView(),
+        },
+        {
+          binding: 2,
+          resource: textures[0].createView(),
+        },
+        {
+          binding: 3,
+          resource: {
+            buffer: buffer0,
+          },
+        },
+      ],
+    });
+  
+    const computeBindGroup1 = device.createBindGroup({
+      layout: blurPipeline.getBindGroupLayout(1),
+      entries: [
+        {
+          binding: 1,
+          resource: textures[0].createView(),
+        },
+        {
+          binding: 2,
+          resource: textures[1].createView(),
+        },
+        {
+          binding: 3,
+          resource: {
+            buffer: buffer1,
+          },
+        },
+      ],
+    });
+  
+    const computeBindGroup2 = device.createBindGroup({
+      layout: blurPipeline.getBindGroupLayout(1),
+      entries: [
+        {
+          binding: 1,
+          resource: textures[1].createView(),
+        },
+        {
+          binding: 2,
+          resource: textures[0].createView(),
+        },
+        {
+          binding: 3,
+          resource: {
+            buffer: buffer0,
+          },
+        },
+      ],
+    });
+  
+    const showResultBindGroup = device.createBindGroup({
+      layout: fullscreenQuadPipeline.getBindGroupLayout(0),
+      entries: [
+        {
+          binding: 0,
+          resource: sampler,
+        },
+        {
+          binding: 1,
+          resource: textures[1].createView(),
+        },
+      ],
+    });
+  
+    const settings = {
+      filterSize: 15,
+      iterations: 2,
+    };
+  
+    let blockDim;
+    const updateSettings = () => {
+      blockDim = tileDim - (settings.filterSize - 1);
+      device.queue.writeBuffer(
+        blurParamsBuffer,
+        0,
+        new Uint32Array([settings.filterSize, blockDim])
+      );
+    };
+ 
+  
+    updateSettings();
+  
+    function frame() {
+  
+      const commandEncoder = device.createCommandEncoder();
+  
+      const computePass = commandEncoder.beginComputePass();
+      computePass.setPipeline(blurPipeline);
+      computePass.setBindGroup(0, computeConstants);
+  
+      computePass.setBindGroup(1, computeBindGroup0);
+      computePass.dispatchWorkgroups(
+        Math.ceil(srcWidth / blockDim),
+        Math.ceil(srcHeight / batch[1])
+      );
+  
+      computePass.setBindGroup(1, computeBindGroup1);
+      computePass.dispatchWorkgroups(
+        Math.ceil(srcHeight / blockDim),
+        Math.ceil(srcWidth / batch[1])
+      );
+  
+      for (let i = 0; i < settings.iterations - 1; ++i) {
+        computePass.setBindGroup(1, computeBindGroup2);
+        computePass.dispatchWorkgroups(
+          Math.ceil(srcWidth / blockDim),
+          Math.ceil(srcHeight / batch[1])
+        );
+  
+        computePass.setBindGroup(1, computeBindGroup1);
+        computePass.dispatchWorkgroups(
+          Math.ceil(srcHeight / blockDim),
+          Math.ceil(srcWidth / batch[1])
+        );
+      }
+  
+      computePass.end();
+  
+      const passEncoder = commandEncoder.beginRenderPass({
+        colorAttachments: [
+          {
+            view: context.getCurrentTexture().createView(),
+            clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+            loadOp: 'clear',
+            storeOp: 'store',
+          },
+        ],
+      });
+  
+      passEncoder.setPipeline(fullscreenQuadPipeline);
+      passEncoder.setBindGroup(0, showResultBindGroup);
+      passEncoder.draw(6, 1, 0, 0);
+      passEncoder.end();
+      device.queue.submit([commandEncoder.finish()]);
+  
+      requestAnimationFrame(frame);
+    }
     requestAnimationFrame(frame);
   }
-  requestAnimationFrame(frame);
 
-  }
   export default postProcessing;
