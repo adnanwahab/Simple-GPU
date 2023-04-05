a42bfc07e79e10a15e691bd965313f84e70a3027
_ <93497294+shelbernstein@users.noreply.github.com>
2023-04-05 12:10:57 -0500

    try stuff

--- obj/obj-reducer.js
+++ obj/obj-reducer.js
@@ -1,40 +1,49 @@
 let fs = require('fs').promises
 let f = require('fs')
+const path = require('path');
 
 
-let folders = ["/1", "/2", "/3", "/4", "/5"].slice(0, 1)
+let folders = ["/1/", "/2/", "/3/", "/4/", "/5/"]
 
 async function doStuff () {
-  folders.forEach(async (dir) => {
-    for (let i = 0; i < 200; i++) {
-    writeShit(dir, i)
-  }
-})
+  folders.forEach(async d => {
+    let files = (await fs.readdir(__dirname + d)).filter(file => (path.extname(file) === '.obj'));
+    files.forEach(function (file, i) {
+      //console.log(i)
+      writeShit(file, d)
+      //console.log(__dirname + d, files)
+  });
+  f.writeFileSync(__dirname + d + 'meta.json', JSON.stringify({files: files.length}))
+  })
 }
 
 doStuff()
 
 
-async function writeShit (dir, i) {
-  
-  let file = __dirname + dir +  `/${i}myfile.obj`;
-  const writeTo = __dirname + dir + `/${i}myfile.bin`;
+async function writeShit (file, dir) {
+  file = __dirname + dir +  file;
+   const writeTo = 
+  file.substr(0, file.lastIndexOf(".")) + ".bin";
   //console.log(writeTo)
   //console.log(file)
+  let hasPassed = 0
+  //o Beta_Joints_Beta_Joints.005
   let data = await fs.readFile(file)
     let lines = data.toString().split('\n')
     let frame = []
+    
     lines.forEach((line, idx) => {
-      if (line[0] === 'v'&& line[1] === ' ') {
+      if (line[0] === 'o') hasPassed+= 1
+      if (line[0] === 'v'&& line[1] === ' ' && hasPassed===2) {
         //if (idx=== 4)
-        console.log(line)
+        //console.log(line)
         frame.push(line.slice(2).split(' ')
         .map(parseFloat).concat(0))
       }
       
       
     })
-    console.log(lines.length)
+    //console.log(lines.length)
     //if (i === 0) frame.forEach(console.log)
 
     let numbers = frame.flat()
@@ -54,6 +63,7 @@ async function writeShit (dir, i) {
     // wstream.write(b);
     // wstream.end();
     //console.log(writeTo)
+    console.log(writeTo)
     f.writeFileSync(writeTo, b)
   // await fs.writeFile(writeTo,
   //   b, function (err, data) {
