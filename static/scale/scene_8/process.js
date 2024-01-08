bbbb669ebe42d4ed68611084d3d03629e9a761f2
shel bernstein <shel.bernstein123@gmail.com>
2024-01-08 13:52:57 -0600

    cool beans

--- static/scale/scene_8/process.js
+++ static/scale/scene_8/process.js
@@ -1,20 +1,32 @@
 const fs = require('fs');
 
+
 let data = JSON.parse(fs.readFileSync('1.json'));
 
-const typedArray = new Float32Array(data.points.length * 4);
 
-for (let i = 0; i < data.points.length; i += 4) {
-  typedArray[i] = data.points[i].x;
-  typedArray[i] = data.points[i].y;
-  typedArray[i] = data.points[i].z;
 
-}
+// Simulating reading JSON data (replace this with actual file reading)
+const jsonData = data
 
-// Specify the file name
-const fileName = 'typedArrayFile.bin';
+// Extract points data
+const points = jsonData.points;
 
-// Write the typed array to the file
-fs.writeFileSync(fileName, typedArray);
+// Create a typed array
+const numFields = 5; // x, y, z, i, d
+const typedArray = new Float32Array(points.length * numFields);
 
-console.log(`Typed array has been written to ${fileName}`);
+// Populate the typed array
+points.forEach((point, index) => {
+  typedArray[index * numFields] = point.x;
+  typedArray[index * numFields + 1] = point.y;
+  typedArray[index * numFields + 2] = point.z;
+  typedArray[index * numFields + 3] = point.i;
+  typedArray[index * numFields + 4] = point.d;
+});
+
+// Write the binary data to a file
+const buffer = Buffer.from(typedArray.buffer);
+fs.writeFile('points.bin', buffer, (err) => {
+  if (err) throw err;
+  console.log('The binary file has been saved!');
+});
